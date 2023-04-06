007bfc7e8c19fe99ae8895634c738c772b17e0f1
Albert <albert990624@gmail.com>
2023-04-06 16:55:30 +0800

    feat(go): add rename.sh

---
+++ leetcode/go/rename.sh
@@ -0,0 +1,20 @@
+rename_go () {
+    local root_dir="/Users/albert/CodeSpace/learn/my-learn-algorithm/leetcode/go"
+    for dir in ${root_dir}/*/; do
+        local dirname="$(basename ${dir})"
+        local solution_file="${dir}solution.go"
+        local question_file="${dir}question.md"
+        if [[ ${dirname} =~ ^([0-9]{4}).*$ ]]; then
+            local prefix="${BASH_REMATCH[1]}"
+            local new_name="${root_dir}/${dirname}/lc-${prefix}"
+            if [[ && -f "${solution_file}"]]; then
+                mv "${solution_file}" "${new_name}.go"
+            fi
+            if [[ && -f "${question_file}"]]; then
+                mv "{question_file}" "${new_name}.md"
+            fi
+        fi
+    done
+}
+
+rename_go
