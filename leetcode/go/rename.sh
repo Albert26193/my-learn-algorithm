b19bb2803b9908d378e7fe8a5a98837a23b2a608
Albert <albert990624@gmail.com>
2023-04-12 19:08:55 +0800

    fix(go): rename question.md to lc-{num}.md

--- leetcode/go/rename.sh
+++ leetcode/go/rename.sh
@@ -7,11 +7,12 @@ rename_go () {
         if [[ ${dirname} =~ ^([0-9]{4}).*$ ]]; then
             local prefix="${BASH_REMATCH[1]}"
             local new_name="${root_dir}/${dirname}/lc-${prefix}"
-            if [[ && -f "${solution_file}"]]; then
-                mv "${solution_file}" "${new_name}.go"
-            fi
-            if [[ && -f "${question_file}"]]; then
-                mv "{question_file}" "${new_name}.md"
+            # if [[ -f "${solution_file}"]]; then
+            #     mv "${solution_file}" "${new_name}.go"
+            # fi
+            if [[ -f "${question_file}" ]]; then
+                mv "${question_file}" "${new_name}.md"
+                echo "${new_name}.md"
             fi
         fi
     done
