8ac8c5f39e2676a75f789a6d662dfdab678b5319
Albert <albert990624@gmail.com>
2023-04-19 00:41:20 +0800

    feat(go): add 0046 backTrack

---
+++ leetcode/go/0046.permutations/solution.go
@@ -0,0 +1,51 @@
+// Created by Albert at 2023/04/19 00:29
+// https://leetcode.cn/problems/permutations/
+
+package main
+
+import (
+	"fmt"
+
+	. "github.com/j178/leetgo/testutils/go"
+)
+
+// @lc code=begin
+
+func backTrack(nums []int, curIndex int, combination []int, ans *[][]int, vis []int) {
+	if curIndex > len(nums) {
+		return
+	}
+
+	if curIndex == len(nums) {
+		*ans = append(*ans, combination)
+		return
+	}
+
+	for i := range nums {
+		if vis[i] == 1 {
+			continue
+		}
+
+		vis[i] = 1
+		combination = append(combination, i+1)
+		backTrack(nums, curIndex+1, combination, ans, vis)
+		combination = combination[:len(combination)-1]
+		vis[i] = 0
+	}
+}
+
+func permute(nums []int) (ans [][]int) {
+	vis := make([]int, len(nums))
+	backTrack(nums, 0, []int{}, &ans, vis)
+	return
+}
+
+// @lc code=end
+
+func main() {
+	//stdin := bufio.NewReader(os.Stdin)
+	//nums := Deserialize[[]int](ReadLine(stdin))
+	nums := []int{1, 2, 3}
+	ans := permute(nums)
+	fmt.Println("output: " + Serialize(ans))
+}
---
+++ leetcode/go/1026.maximum-difference-between-node-and-ancestor/solution.go
@@ -0,0 +1,71 @@
+// Created by Albert at 2023/04/18 23:26
+// https://leetcode.cn/problems/maximum-difference-between-node-and-ancestor/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	. "github.com/j178/leetgo/testutils/go"
+)
+
+// @lc code=begin
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func abs(a int) int {
+	if a > 0 {
+		return a
+	} else {
+		return -a
+	}
+}
+
+func dfs(root *TreeNode, minVal int, maxVal int) int {
+	if root == nil {
+		return 0
+	}
+
+	diff := max(abs(root.Val-minVal), abs(root.Val-maxVal))
+	minVal = min(minVal, root.Val)
+	maxVal = max(maxVal, root.Val)
+	diff = max(diff, dfs(root.Left, minVal, maxVal))
+	diff = max(diff, dfs(root.Right, minVal, maxVal))
+	return diff
+}
+
+func maxAncestorDiff(root *TreeNode) (ans int) {
+	return dfs(root, root.Val, root.Val)
+}
+
+// @lc code=end
+
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+	root := Deserialize[*TreeNode](ReadLine(stdin))
+	ans := maxAncestorDiff(root)
+	fmt.Println("output: " + Serialize(ans))
+}
