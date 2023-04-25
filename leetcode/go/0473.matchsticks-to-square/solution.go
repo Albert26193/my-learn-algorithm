6e919342bf523a0c24bc23559b8f71083de83ac6
Albert <albert990624@gmail.com>
2023-04-26 06:53:26 +0800

    feat(go): add 473(backTack and advance pruning)

--- leetcode/go/0473.matchsticks-to-square/solution.go
+++ leetcode/go/0473.matchsticks-to-square/solution.go
@@ -4,24 +4,65 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"sort"
 
 	. "github.com/j178/leetgo/testutils/go"
 )
 
 // @lc code=begin
 // TODO
-func makesquare(matchsticks []int) bool {
+// backTrack index means: from index To the end of matchsticks array, 
+// all of the matchsticks can be put into eligible edge
+func backTrack(matchsticks []int, stickIndex int, edges []int, target int) bool {
+	if stickIndex >= len(matchsticks) {
+		return true
+	}
 
+	for i := 0; i < 4; i++ {
+		if edges[i]+matchsticks[stickIndex] > target {
+			continue
+		}
+		edges[i] += matchsticks[stickIndex]
+		if backTrack(matchsticks, stickIndex+1, edges, target) {
+			return true
+		}
+		edges[i] -= matchsticks[stickIndex]
+	}
+
+	return false
+}
+
+func makesquare(matchsticks []int) bool {
+	sticksSum := 0
+	for i := range matchsticks {
+		sticksSum += matchsticks[i]
+	}
+
+	if sticksSum%4 != 0 {
+		return false
+	}
+
+	target := sticksSum / 4
+	// sort.Ints(matchsticks)
+
+	// why descending sort is better than ascending sort in TIME ?
+	// pruning in advance
+	sort.Slice(matchsticks, func(i, j int) bool {
+		return matchsticks[i] > matchsticks[j]
+	})
+
+	edges := make([]int, 4)
+	return backTrack(matchsticks, 0, edges, target)
 }
 
 // @lc code=end
 
 func main() {
-	stdin := bufio.NewReader(os.Stdin)
-	matchsticks := Deserialize[[]int](ReadLine(stdin))
+	// stdin := bufio.NewReader(os.Stdin)
+	// matchsticks := Deserialize[[]int](ReadLine(stdin))
+	matchsticks := []int{1, 1, 2, 2, 2}
+	matchsticks = []int{5, 5, 5, 5, 16, 4, 4, 4, 4, 4, 3, 3, 3, 3, 4}
 	ans := makesquare(matchsticks)
 	fmt.Println("output: " + Serialize(ans))
 }
--- leetcode/go/1109.corporate-flight-bookings/solution.go
+++ leetcode/go/1109.corporate-flight-bookings/solution.go
@@ -4,9 +4,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 
 	. "github.com/j178/leetgo/testutils/go"
 )
@@ -15,31 +13,33 @@ import (
 
 func corpFlightBookings(bookings [][]int, n int) (ans []int) {
 	diff := make([]int, n+1)
-	ans = make([]int, n+1)
+	ans = make([]int, n)
 	for _, singleBooking := range bookings {
-		startIndex := singleBooking[0]
-		endIndex := singleBooking[1]
+		startIndex := singleBooking[0] - 1
+		endIndex := singleBooking[1] - 1
 		ticketCount := singleBooking[2]
 
 		diff[startIndex] += ticketCount
-		diff[endIndex] -= ticketCount
+		diff[endIndex+1] -= ticketCount
 	}
 
-	for i := range diff {
+	for i := 0; i < n; i++ {
 		if i == 0 {
 			ans[0] = diff[0]
 		} else {
 			ans[i] = diff[i] + ans[i-1]
 		}
 	}
+
 	return
 }
 
 // @lc code=end
 func main() {
-	stdin := bufio.NewReader(os.Stdin)
-	bookings := Deserialize[[][]int](ReadLine(stdin))
-	n := Deserialize[int](ReadLine(stdin))
+	// stdin := bufio.NewReader(os.Stdin)
+	// bookings := Deserialize[[][]int](ReadLine(stdin))
+	bookings := [][]int{{1, 2, 10}, {2, 3, 20}, {2, 5, 25}}
+	n := 5
 	ans := corpFlightBookings(bookings, n)
 	fmt.Println("output: " + Serialize(ans))
 }
