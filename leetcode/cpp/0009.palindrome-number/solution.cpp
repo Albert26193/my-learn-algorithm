c43b712cf9fcfe8d9b9d2f7090d846d41f55b626
Albert <albert990624@gmail.com>
2023-06-24 21:26:23 +0800

    feat: add c++ debug settings

--- leetcode/cpp/0009.palindrome-number/solution.cpp
+++ leetcode/cpp/0009.palindrome-number/solution.cpp
@@ -2,6 +2,10 @@
 // https://leetcode.cn/problems/palindrome-number/
 
 #include <iostream>
+
+#include "../cpp_headers/LC_HEADERS.h"
+#include "../cpp_headers/LC_IO.h"
+
 using namespace std;
 
 // @lc code=begin
@@ -11,6 +15,9 @@ using namespace std;
 int main() {
     for (int i = 0; i < 100; i++) {
         cout << i << endl;
+        cout << i + 1 << endl;
     }
+    vector<int> nums(100, 10);
+    cout << nums << endl;
     return 0;
 }
