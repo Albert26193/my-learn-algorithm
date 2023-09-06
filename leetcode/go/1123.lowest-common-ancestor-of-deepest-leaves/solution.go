4ebc6cb855b72e43e78cb787a234320c36954cd4
Albert <albert990624@gmail.com>
2023-09-06 16:56:18 +0800

    feat: Add solution for 1123. Lowest Common Ancestor of Deepest Leaves

---
+++ leetcode/go/1123.lowest-common-ancestor-of-deepest-leaves/solution.go
@@ -0,0 +1,52 @@
+// Created by Albert at 2023/09/06 14:50
+// leetgo: 1.3.7
+// https://leetcode.cn/problems/lowest-common-ancestor-of-deepest-leaves/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	. "github.com/j178/leetgo/testutils/go"
+)
+
+// @lc code=begin
+
+func lcaDeepestLeaves(root *TreeNode) *TreeNode {
+	_, ans := findDeepestNode(root)
+	return ans
+}
+
+func findDeepestNode(root *TreeNode) (int, *TreeNode) {
+	if root == nil {
+		return 0, nil
+	}
+
+	// why error?
+	if root.Left == nil && root.Right == nil {
+		return 1, root
+	}
+
+	depthLeft, leftLcaNode := findDeepestNode(root.Left)
+	depthRight, rightLcaNode := findDeepestNode(root.Right)
+
+	if depthLeft < depthRight {
+		return depthRight + 1, rightLcaNode
+	} else if depthLeft > depthRight {
+		return depthLeft + 1, leftLcaNode
+	} else {
+		return depthLeft + 1, root
+	}
+}
+
+// @lc code=end
+
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+	root := Deserialize[*TreeNode](ReadLine(stdin))
+	ans := lcaDeepestLeaves(root)
+
+	fmt.Println("\noutput:", Serialize(ans))
+}
