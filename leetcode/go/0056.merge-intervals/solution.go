e03aa230e25410193cee2881058e9df06f17786c
Albert <albert990624@gmail.com>
2023-09-13 08:45:35 +0800

    feat: implement merge function for LC.56 merging intervals

--- leetcode/go/0056.merge-intervals/solution.go
+++ leetcode/go/0056.merge-intervals/solution.go
@@ -14,7 +14,7 @@ import (
 )
 
 // @lc code=begin
-
+/*
 func merge(intervals [][]int) (ans [][]int) {
 	sort.Slice(intervals, func(i, j int) bool {
 		if intervals[i][0] == intervals[j][0] {
@@ -42,6 +42,45 @@ func merge(intervals [][]int) (ans [][]int) {
 	ans = append(ans, []int{startPoint, endPoint})
 
 	return
+}
+*/
+
+// TODO: implement it again
+func max(a int, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func merge(intervals [][]int) (ans [][]int) {
+	sort.Slice(intervals, func(i, j int) bool {
+		if intervals[i][0] == intervals[j][0] {
+			return intervals[i][1] < intervals[j][1]
+		} else {
+			return intervals[i][0] < intervals[j][0]
+		}
+	})
+
+	idx := -1
+
+	for i := 0; i < len(intervals); i++ {
+		ans = append(ans, []int{0, 0})
+	}
+
+	for _, interval := range intervals {
+		// if don't need to merge
+		if (idx == -1) || (interval[0] > ans[idx][1]) {
+			idx += 1
+			ans[idx] = interval
+		} else {
+			// if need to merge
+			ans[idx][1] = max(ans[idx][1], interval[1])
+		}
+	}
+
+	return ans[:idx+1]
 }
 
 // @lc code=end
