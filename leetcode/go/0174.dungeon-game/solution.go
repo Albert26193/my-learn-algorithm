09a73b02aedb44a7eb71f8abe0edb22263620a20
Albert <albert990624@gmail.com>
2023-09-19 14:55:50 +0800

    refactor: optimize 174.dungeon game solution

--- leetcode/go/0174.dungeon-game/solution.go
+++ leetcode/go/0174.dungeon-game/solution.go
@@ -7,6 +7,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 
 	. "github.com/j178/leetgo/testutils/go"
@@ -23,8 +24,38 @@ func maxx(a int, b int) int {
 	return b
 }
 
+func minn(a int, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
 func calculateMinimumHP(dungeon [][]int) (ans int) {
-	return
+	row := len(dungeon)
+	col := len(dungeon[0])
+
+	dp := make([][]int, row+1)
+	for i := 0; i < row+1; i++ {
+		dp[i] = make([]int, col+1)
+		for j := 0; j < col+1; j++ {
+			dp[i][j] = math.MaxInt32
+		}
+	}
+
+	// 此处可以看成公主真正的位置,也是 dp 的出发点
+	// 出发点必须是1, 因为一旦到达0就无法进行下一步行动了
+	// dp[i][j] 可以看成到 "公主" 位置所需要的最小值
+	dp[row][col-1], dp[row-1][col] = 1, 1
+	for i := row - 1; i >= 0; i-- {
+		for j := col - 1; j >= 0; j-- {
+			minCost := minn(dp[i][j+1], dp[i+1][j])
+			dp[i][j] = maxx(minCost-dungeon[i][j], 1)
+		}
+	}
+
+	return dp[0][0]
 }
 
 // @lc code=end
