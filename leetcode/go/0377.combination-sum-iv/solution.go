8eb0dfe78bff1fb2989460ad2f46513b209159ef
Albert <albert990624@gmail.com>
2023-10-02 21:39:18 +0800

    feat: add solution for leetcode problem #377

---
+++ leetcode/go/0377.combination-sum-iv/solution.go
@@ -0,0 +1,50 @@
+// Created by Albert at 2023/10/02 16:52
+// leetgo: 1.3.8
+// https://leetcode.cn/problems/combination-sum-iv/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	. "github.com/j178/leetgo/testutils/go"
+)
+
+// @lc code=begin
+
+func combinationSum4(nums []int, target int) (ans int) {
+	dp := make([]int, target+1)
+
+	dp[0] = 1
+	for _, num := range nums {
+		for i := num; i <= target; i++ {
+			dp[i] += dp[i-num]
+		}
+	}
+
+	return dp[target]
+}
+
+// @lc code=end
+
+func main() {
+	file, err := os.Open("./testcases.txt")
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	// skip first line
+	ReadLine(reader)
+
+	nums := Deserialize[[]int](ReadLine(reader))
+	target := Deserialize[int](ReadLine(reader))
+	ans := combinationSum4(nums, target)
+
+	fmt.Println("\noutput:", Serialize(ans))
+}
