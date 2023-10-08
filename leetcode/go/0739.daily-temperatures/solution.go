e9b7168e0899a33e2a4e6052bf3ca770d2be57d4
Albert <albert990624@gmail.com>
2023-10-08 12:37:10 +0800

    feat: Add solution for problem 739

---
+++ leetcode/go/0739.daily-temperatures/solution.go
@@ -0,0 +1,52 @@
+// Created by Albert at 2023/10/08 12:03
+// leetgo: 1.3.8
+// https://leetcode.cn/problems/daily-temperatures/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	. "github.com/j178/leetgo/testutils/go"
+)
+
+// @lc code=begin
+
+func dailyTemperatures(temperatures []int) (ans []int) {
+	stack := []int{}
+	ans = make([]int, len(temperatures))
+	for i, temp := range temperatures {
+		for len(stack) > 0 && temperatures[stack[len(stack)-1]] < temp {
+			stackTop := stack[len(stack)-1]
+			ans[stackTop] = i - stackTop
+
+			stack = stack[:len(stack)-1]
+		}
+
+		stack = append(stack, i)
+	}
+	return
+}
+
+// @lc code=end
+
+func main() {
+	file, err := os.Open("./testcases.txt")
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	// skip first line
+	ReadLine(reader)
+
+	temperatures := Deserialize[[]int](ReadLine(reader))
+	ans := dailyTemperatures(temperatures)
+
+	fmt.Println("\noutput:", Serialize(ans))
+}
