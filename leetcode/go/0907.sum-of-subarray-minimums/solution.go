07df74be3bea6a00aec31d12d87e5495bd20293f
Albert <albert990624@gmail.com>
2023-10-08 13:19:18 +0800

    refactor: Refactor sumSubarrayMins function and main function

--- leetcode/go/0907.sum-of-subarray-minimums/solution.go
+++ leetcode/go/0907.sum-of-subarray-minimums/solution.go
@@ -13,19 +13,70 @@ import (
 )
 
 // @lc code=begin
-// TODO: monotonic stack + multiplication principle
 func sumSubarrayMins(arr []int) (ans int) {
 	const mod int = 1e9 + 7
 
-	ans = (ans%mod + mod) % mod
+	arrLength := len(arr)
+	stack := []int{}
+
+	recordLeft := make([]int, arrLength)
+	recordRight := make([]int, arrLength)
+
+	for i := 0; i < arrLength; i++ {
+		for (len(stack) > 0) && (arr[i] < arr[stack[len(stack)-1]]) {
+			stack = stack[:len(stack)-1]
+		}
+
+		if len(stack) == 0 {
+			recordLeft[i] = -1
+		} else {
+			recordLeft[i] = stack[len(stack)-1]
+		}
+
+		stack = append(stack, i)
+	}
+
+	stack = []int{}
+	for i := arrLength - 1; i >= 0; i-- {
+		// why <= ? ==> to filter first left Num which equals arr[i]
+		for (len(stack) > 0) && (arr[i] <= arr[stack[len(stack)-1]]) {
+			stack = stack[:len(stack)-1]
+		}
+
+		if len(stack) == 0 {
+			recordRight[i] = arrLength
+		} else {
+			recordRight[i] = stack[len(stack)-1]
+		}
+
+		stack = append(stack, i)
+	}
+
+	// fmt.Println(recordLeft, recordRight)
+	for i := range arr {
+		sum := (arr[i]*(i-recordLeft[i])*(recordRight[i]-i)%mod + mod) % mod
+		ans = ((ans+sum)%mod + mod) % mod
+	}
+
 	return
 }
 
 // @lc code=end
 
 func main() {
-	stdin := bufio.NewReader(os.Stdin)
-	arr := Deserialize[[]int](ReadLine(stdin))
+	file, err := os.Open("./testcases.txt")
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	// skip first line
+	ReadLine(reader)
+
+	arr := Deserialize[[]int](ReadLine(reader))
 	ans := sumSubarrayMins(arr)
 
 	fmt.Println("\noutput:", Serialize(ans))
