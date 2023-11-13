63bdedbb3014d3a37cd522c3021b92be11eaa063
Albert <albert990624@gmail.com>
2023-11-13 15:28:45 +0800

    refactor: Remove unnecessary white-space and comments

--- acwing/acwing-boost/1.dp/acwing.1015/cpp/acwing.1015.cpp
+++ acwing/acwing-boost/1.dp/acwing.1015/cpp/acwing.1015.cpp
@@ -1,6 +1,6 @@
+#include <algorithm>
 #include <cstdio>
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
@@ -13,20 +13,20 @@ int f[N][N];
 int main() {
     int T = 0;
     scanf("%d", &T);
-    while (T --) {
-        scanf ("%d%d", &n, &m);
-        for (int i = 1; i <=n; i++) {
-            for (int j = 1; j <=m; j++) {
+    while (T--) {
+        scanf("%d%d", &n, &m);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
                 scanf("%d", &w[i][j]);
             }
         }
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <=m ; j++) {
+            for (int j = 1; j <= m; j++) {
                 f[i][j] = max(f[i - 1][j], f[i][j - 1]) + w[i][j];
             }
         }
-        
+
         printf("%d\n", f[n][m]);
     }
 
