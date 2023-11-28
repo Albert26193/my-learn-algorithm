1c036f49c697c665d92011312d1e08ef5629cf7d
Albert <albert990624@gmail.com>
2023-11-28 16:44:19 +0800

    feat: add solution for knapsack problem

--- acwing/go/acw3/acw3.go
+++ acwing/go/acw3/acw3.go
@@ -1,1 +1,58 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// f[j] ==> sum of volume <= j, max worth is f[j]
+// f[j] can be transfered from f[j - v[0]], f[j - v[1]], ... ==> O(n^2)
+
+func main() {
+	// read
+	// file, err := os.Open("./testcases.txt")
+	// if err != nil {
+	// 	fmt.Println("Error", err)
+	// 	return
+	// }
+	//
+	// in := bufio.NewReader(file)
+	// defer file.Close()
+
+	in := bufio.NewReader(os.Stdin)
+	var N, V int
+	fmt.Fscan(in, &N)
+	fmt.Fscan(in, &V)
+
+	var v = make([]int, N)
+	var w = make([]int, N)
+
+	for i := 0; i < N; i++ {
+		fmt.Fscan(in, &v[i])
+		fmt.Fscan(in, &w[i])
+	}
+
+	// dp
+	f := make([]int, V+1)
+	for i := 0; i < N; i++ {
+		currentCost := v[i]
+		for j := v[i]; j <= V; j++ {
+			f[j] = maxx(f[j], f[j-currentCost]+w[i])
+		}
+	}
+
+	fmt.Println(f[V])
+}
+
+func maxx(nums ...int) int {
+	res := nums[0]
+
+	for _, v := range nums {
+		if v > res {
+			res = v
+		}
+	}
+
+	return res
+}
