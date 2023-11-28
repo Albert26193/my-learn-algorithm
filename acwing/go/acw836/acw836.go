feb3a6962ed1aa19419efd370ea0030f18a4902d
Albert <albert990624@gmail.com>
2023-11-29 01:13:24 +0800

    feat(acw836): add solution for acw836 problem

---
+++ acwing/go/acw836/acw836.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("./testcases.txt")
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	in := bufio.NewReader(file)
+	defer file.Close()
+
+	var n, m int
+	fmt.Fscan(in, &n)
+	fmt.Fscan(in, &m)
+
+	uf := newUnionFind(n + 1)
+
+	for i := 0; i < m; i++ {
+		var ex string
+		var a, b int
+		fmt.Fscan(in, &ex)
+		fmt.Fscan(in, &a)
+		fmt.Fscan(in, &b)
+		if ex == "M" {
+			uf.union(a, b)
+		} else if ex == "Q" {
+			if uf.connected(a, b) {
+				fmt.Println("Yes")
+			} else {
+				fmt.Println("No")
+			}
+		}
+	}
+
+}
+
+type unionFind struct {
+	parent []int
+	size   []int
+	count  int
+}
+
+func newUnionFind(n int) *unionFind {
+	parent := make([]int, n)
+	size := make([]int, n)
+
+	for i := range parent {
+		parent[i] = i
+		size[i] = 1
+	}
+
+	count := n
+
+	return &unionFind{
+		parent: parent,
+		size:   size,
+		count:  count,
+	}
+}
+
+func (uf *unionFind) find(x int) int {
+	if uf.parent[x] != x {
+		uf.parent[x] = uf.find(uf.parent[x])
+	}
+
+	return uf.parent[x]
+}
+
+func (uf *unionFind) union(x int, y int) bool {
+	xRoot := uf.find(x)
+	yRoot := uf.find(y)
+
+	if xRoot == yRoot {
+		return false
+	}
+
+	if uf.size[xRoot] < uf.size[yRoot] {
+		xRoot, yRoot = yRoot, xRoot
+	}
+
+	uf.parent[yRoot] = xRoot
+	uf.size[xRoot] += uf.size[yRoot]
+	uf.count--
+	return true
+}
+
+func (uf *unionFind) connected(x int, y int) bool {
+	x = uf.find(x)
+	y = uf.find(y)
+	return x == y
+}
