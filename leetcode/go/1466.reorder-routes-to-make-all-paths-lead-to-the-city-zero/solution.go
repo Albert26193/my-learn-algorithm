d4a78c171724ec34f80de2c3c8e0aa2bfc8ce881
Albert's server <albert990624@gmail.com>
2023-12-07 13:23:16 +0800

    feat(leetcode/go/1466): add solution for problem 1466 "Reorder Routes to Make All Paths Lead to the City Zero"
    This commit adds a solution for the problem 1466 "Reorder Routes to Make All Paths Lead to the City Zero" on LeetCode. The problem involves reordering the directions of routes in a city network to ensure that every city can reach city 0. The solution uses a depth-first search (DFS) algorithm to traverse the network and count the number of routes that need to be changed. The implementation is in the file `solution.go`.
    
    The commit also includes a test case file `testcases.txt` with input and expected output for the problem. The main function reads the test cases from the file and calls the `minReorder` function to calculate the result. The output is then printed to the console.

---
+++ leetcode/go/1466.reorder-routes-to-make-all-paths-lead-to-the-city-zero/solution.go
@@ -0,0 +1,75 @@
+// Created by Albert's server at 2023/12/07 12:36
+// leetgo: dev
+// https://leetcode.cn/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+
+	. "github.com/j178/leetgo/testutils/go"
+)
+
+// @lc code=begin
+
+func minReorder(n int, connections [][]int) (ans int) {
+	dia := make([][]int, n)
+	recordDirection := make(map[string]bool)
+
+	for _, v := range connections {
+		start := v[0]
+		end := v[1]
+		dia[start] = append(dia[start], end)
+		dia[end] = append(dia[end], start)
+		direction := strconv.Itoa(start) + "#" + strconv.Itoa(end)
+		recordDirection[direction] = true
+	}
+
+	vis := make([]int, n)
+	var dfs func(int, int)
+	dfs = func(curNode int, faNode int) {
+		nextNodes := dia[curNode]
+		if vis[curNode] == 1 {
+			return
+		}
+
+		vis[curNode] = 1
+
+		relation := strconv.Itoa(curNode) + "#" + strconv.Itoa(faNode)
+		if !recordDirection[relation] {
+			// fmt.Println(curNode, faNode, "fff")
+			ans += 1
+		}
+
+		for _, node := range nextNodes {
+			dfs(node, curNode)
+		}
+	}
+
+	dfs(0, -1)
+	return ans - 1
+}
+
+// @lc code=end
+
+func main() {
+	file, err := os.Open("./testcases.txt")
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	in := bufio.NewReader(file)
+	defer file.Close()
+
+	ReadLine(in)
+	n := Deserialize[int](ReadLine(in))
+	connections := Deserialize[[][]int](ReadLine(in))
+	// fmt.Println(n, connections)
+	ans := minReorder(n, connections)
+
+	fmt.Println("\noutput:", Serialize(ans))
+}
