90f338fb125478cb2b3a7275e3a0ca2999b355ec
Albert's server <albert990624@gmail.com>
2023-12-19 22:47:39 +0800

    chore(acw785.go): Remove unused file acw785.go
    feat(acw786.go): Add new file acw786.go with implementation of quickChoose function using quicksort algorithm
    
    The acw786.go file contains the implementation of the quickChoose function, which uses the quicksort algorithm to find the kth smallest element in an array. The function takes an array, left and right indices, and the value of k as input. It recursively partitions the array based on a pivot element and returns the index of the kth smallest element.
    
    The main function reads input from a file named "testcases.txt" and calls the quickChoose function to find the kth smallest element in the given array. The result is printed to the console.
    
    The testcases.txt file is also added, which contains test cases for the program.

---
+++ acwing/go/acw895/acw895.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("./testcases.txt")
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	in := bufio.NewReader(file)
+	defer file.Close()
+
+	var n int
+	fmt.Fscan(in, &n)
+
+	var arr = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(in, &arr[i])
+	}
+
+	// fmt.Println(arr)
+	f := make([]int, n+1)
+	f[0] = 1
+	for i := 1; i < n; i++ {
+		f[i] = 1
+		for j := 0; j < i; j++ {
+			if arr[i] > arr[j] {
+				f[i] = maxx(f[i], f[j] + 1)
+			}
+		}
+	}
+
+	ans := maxx(f...)
+	fmt.Printf("%d", ans)
+}
+
+func maxx(nums ...int) int {
+	res := nums[0]
+	for _, v := range nums {
+		if v > res {
+			res = v
+		}
+	}
+	return res
+}
