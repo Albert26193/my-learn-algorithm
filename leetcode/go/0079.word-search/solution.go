fc1c068b3cf736159515ad56df7cc7d8d6fa23cd
Albert's server <albert990624@gmail.com>
2023-12-19 15:31:52 +0800

    feat(leetcode/go/0079.word-search): implement word search algorithm
    This commit adds the implementation of the word search algorithm in the solution.go file. The algorithm searches for a given word in a 2D board of characters. It uses a depth-first search (DFS) approach to explore the board and check if the word can be formed by adjacent characters.
    
    The algorithm starts by iterating through each cell in the board and checks if the first character of the word matches the current cell. If it does, it performs a DFS to search for the remaining characters of the word by exploring adjacent cells. The algorithm keeps track of visited cells to avoid revisiting them.
    
    Additionally, the commit includes changes to the testcases.txt file to add test cases for the word search algorithm. The test cases cover scenarios where the word is present in the board and where it is not.
    
    This implementation improves the functionality of the word search algorithm, allowing it to accurately determine if a given word can be formed by adjacent characters in the board.

--- leetcode/go/0079.word-search/solution.go
+++ leetcode/go/0079.word-search/solution.go
@@ -15,15 +15,96 @@ import (
 // @lc code=begin
 
 func exist(board [][]byte, word string) bool {
+	row := len(board)
+	col := len(board[0])
 
+	var d = [4][2]int{
+		{-1, 0},
+		{1, 0},
+		{0, 1},
+		{0, -1},
+	}
+
+	check := func(x int, y int) bool {
+		if x < 0 || x >= row {
+			return false
+		}
+		if y < 0 || y >= col {
+			return false
+		}
+		return true
+	}
+
+	var dfs func(int, int, int)
+	n := len(word)
+
+	vis := make([]bool, row*col)
+	hasWord := false
+
+	dfs = func(depth int, x int, y int) {
+		if depth >= n-1 {
+			hasWord = true
+			return
+		}
+
+		if !check(x, y) || vis[x*col+y] || board[x][y] != word[depth] {
+			return
+		}
+
+		vis[x*col+y] = true
+		for i := 0; i < 4; i++ {
+			nx := x + d[i][0]
+			ny := y + d[i][1]
+			if !check(nx, ny) {
+				continue
+			}
+
+			if vis[nx*col+ny] {
+				continue
+			}
+
+			if board[nx][ny] != word[depth+1] {
+				continue
+			}
+
+			dfs(depth+1, nx, ny)
+		}
+
+		vis[x*col+y] = false
+	}
+
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			if board[i][j] == word[0] {
+				hasWord = false
+				vis = make([]bool, row*col)
+				dfs(0, i, j)
+
+				if hasWord {
+					return true
+				}
+			}
+		}
+	}
+
+	return false
 }
 
 // @lc code=end
 
 func main() {
-	stdin := bufio.NewReader(os.Stdin)
-	board := Deserialize[[][]byte](ReadLine(stdin))
-	word := Deserialize[string](ReadLine(stdin))
+	file, err := os.Open("./testcases.txt")
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	in := bufio.NewReader(file)
+	defer file.Close()
+
+	ReadLine(in)
+	board := Deserialize[[][]byte](ReadLine(in))
+	word := Deserialize[string](ReadLine(in))
 	ans := exist(board, word)
 
 	fmt.Println("\noutput:", Serialize(ans))
