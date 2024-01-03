b4a7ed548a3c043fb542deec29c6edbb0e217b89
Albert's server <albert990624@gmail.com>
2024-01-03 14:53:46 +0800

    feat(leetcode/go/0128.longest-consecutive-sequence): add solution for problem 128 - Longest Consecutive Sequence
    Add a new solution file `solution.go` that implements an algorithm to find the length of the longest consecutive sequence in an unsorted integer array. The algorithm has a time complexity of O(n).
    
    The solution uses a union-find data structure to keep track of the connected components in the array. It iterates over the array twice: first to initialize the union-find data structure, and second to merge consecutive numbers. Finally, it finds the maximum size of the connected components and returns it as the result.
    
    Add a new testcases file `testcases.txt` that contains input and output examples for the problem.

---
+++ leetcode/go/0128.longest-consecutive-sequence/solution.go
@@ -0,0 +1,83 @@
+// Created by Albert's server at 2024/01/03 13:19
+// leetgo: dev
+// https://leetcode.cn/problems/longest-consecutive-sequence/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	. "github.com/j178/leetgo/testutils/go"
+)
+
+// @lc code=begin
+
+type unionFind struct {
+	fa   map[int]int
+	size map[int]int
+}
+
+func (uf *unionFind) find(x int) int {
+	if uf.fa[x] != x {
+		uf.fa[x] = uf.find(uf.fa[x])
+	}
+
+	return uf.fa[x]
+}
+
+func (uf *unionFind) merge(x int, y int) {
+	fax := uf.find(x)
+	fay := uf.find(y)
+	if fax != fay {
+		uf.fa[y] = fax
+		uf.size[fax] += uf.size[fay]
+	}
+
+}
+
+func longestConsecutive(nums []int) (ans int) {
+	uf := &unionFind{
+		fa:   make(map[int]int),
+		size: make(map[int]int),
+	}
+
+	for _, num := range nums {
+		uf.fa[num] = num
+		uf.size[num] = 1
+	}
+
+	for _, num := range nums {
+		if _, ok := uf.fa[num+1]; ok {
+			uf.merge(num, num+1)
+		}
+	}
+
+	ans = 0
+	for _, num := range nums {
+		if uf.size[num] > ans {
+			ans = uf.size[num]
+		}
+	}
+	return
+}
+
+// @lc code=end
+
+func main() {
+	file, err := os.Open("./testcases.txt")
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	in := bufio.NewReader(file)
+	defer file.Close()
+
+	ReadLine(in)
+	nums := Deserialize[[]int](ReadLine(in))
+	ans := longestConsecutive(nums)
+
+	fmt.Println("\noutput:", Serialize(ans))
+}
