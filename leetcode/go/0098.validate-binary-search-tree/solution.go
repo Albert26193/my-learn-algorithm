cc64d9e0beece9de45fee46b94df512befa8f7ce
Albert's server <albert990624@gmail.com>
2024-01-06 21:32:06 +0800

    feat(leetcode/go/0098.validate-binary-search-tree): add solution for validating a binary search tree
    This commit adds a solution for the problem "98. 验证二叉搜索树" on LeetCode. The problem asks to determine whether a given binary tree is a valid binary search tree.
    
    The solution is implemented in the `solution.go` file. It introduces a function `isValidBST` that takes the root of the binary tree as input and returns a boolean value indicating whether the tree is a valid binary search tree.
    
    The function uses a depth-first search (DFS) approach to recursively check each node in the tree. It maintains two parameters, `lower` and `higher`, which represent the lower and upper bounds for the values in the left and right subtrees, respectively. The function compares the value of each node with the bounds and recursively calls itself for the left and right child nodes, updating the bounds accordingly.
    
    The commit also includes a `testcases.txt` file that contains test cases for the problem. The `main` function in `solution.go` reads the test cases from the file and applies the `isValidBST` function to each input, printing the corresponding output.
    
    The solution has been tested and produces the expected results for the provided test cases.

---
+++ leetcode/go/0098.validate-binary-search-tree/solution.go
@@ -0,0 +1,52 @@
+// Created by Albert's server at 2024/01/06 21:11
+// leetgo: dev
+// https://leetcode.cn/problems/validate-binary-search-tree/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+
+	. "github.com/j178/leetgo/testutils/go"
+)
+
+// @lc code=begin
+
+func isValidBST(root *TreeNode) bool {
+	var dfs func(node *TreeNode, lower int, higher int) bool
+	dfs = func(node *TreeNode, lower int, higher int) bool {
+		if node == nil {
+			return true
+		}
+
+		if node.Val <= lower || node.Val >= higher {
+			return false
+		}
+
+		return dfs(node.Left, lower, node.Val) && dfs(node.Right, node.Val, higher)
+	}
+
+	return dfs(root, math.MinInt32, math.MaxInt32)
+}
+
+// @lc code=end
+
+func main() {
+	file, err := os.Open("./testcases.txt")
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	in := bufio.NewReader(file)
+	defer file.Close()
+
+	ReadLine(in)
+	root := Deserialize[*TreeNode](ReadLine(in))
+	ans := isValidBST(root)
+
+	fmt.Println("\noutput:", Serialize(ans))
+}
