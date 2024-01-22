dfe87d90a89970bc1688648f222a1df3ca125a52
Albert's server <albert990624@gmail.com>
2024-01-22 13:44:04 +0800

    fix(solution.go): refactor trap function to improve readability and performance fix(solution.go): change variable name from stdin to in for consistency fix(solution.go): change file open mode from os.Stdin to "./testcases.txt" for proper input handling

--- leetcode/go/0042.trapping-rain-water/solution.go
+++ leetcode/go/0042.trapping-rain-water/solution.go
@@ -15,15 +15,53 @@ import (
 // @lc code=begin
 
 func trap(height []int) (ans int) {
+	n := len(height)
 
+	// left[i] = j ==> all nums left i, max is j
+	left, right := make([]int, n), make([]int, n)
+
+	leftMax, rightMax := 0, 0
+	for i := 0; i < n; i++ {
+		curLeft, curRight := height[i], height[n-1-i]
+		if curLeft > leftMax {
+			leftMax = curLeft
+		}
+		left[i] = leftMax
+		if curRight > rightMax {
+			rightMax = curRight
+		}
+		right[n-1-i] = rightMax
+	}
+
+	for i := 0; i < n; i++ {
+		ans += (minn(left[i], right[i]) - height[i])
+	}
 	return
+}
+
+func minn(nums ...int) int {
+	res := nums[0]
+	for _, v := range nums {
+		if v < res {
+			res = v
+		}
+	}
+	return res
 }
 
 // @lc code=end
 
 func main() {
-	stdin := bufio.NewReader(os.Stdin)
-	height := Deserialize[[]int](ReadLine(stdin))
+	file, err := os.Open("./testcases.txt")
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	in := bufio.NewReader(file)
+	defer file.Close()
+	ReadLine(in)
+	height := Deserialize[[]int](ReadLine(in))
 	ans := trap(height)
 
 	fmt.Println("\noutput:", Serialize(ans))
