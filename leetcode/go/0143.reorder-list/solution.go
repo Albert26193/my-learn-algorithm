f9c9f2115dc1f95a8445266ab91a7303698606fb
Albert's server <albert990624@gmail.com>
2024-02-20 17:30:17 +0800

    feat(reorder-list): implement the algorithm to reorder a singly linked list
    This commit adds the implementation of the `reorderList` function in the `solution.go` file. The function takes a singly linked list as input and reorders it according to the specified rules. The algorithm consists of the following steps:
    
    1. Find the middle node of the linked list.
    2. Split the linked list into two halves at the middle node.
    3. Reverse the second half of the linked list.
    4. Join the two halves by alternating nodes from each half.
    
    The `reorderList` function uses helper functions `findMiddleNode`, `reverseList`, and `joinList` to perform these steps. The function also includes test cases from the `testcases.txt` file to validate the implementation.
    
    This implementation allows the linked list to be reordered in-place, without changing the values of the nodes. The commit also includes the necessary imports and variable declarations for the solution.
    
    The purpose of this change is to provide a solution for the "Reorder List" problem on LeetCode, which requires reordering a linked list according to a specific pattern.

--- leetcode/go/0143.reorder-list/solution.go
+++ leetcode/go/0143.reorder-list/solution.go
@@ -14,15 +14,81 @@ import (
 
 // @lc code=begin
 
-func reorderList(head *ListNode)  {
+// 1. find middle node
+// 2. split and reverse
+// 3. join List
+func findMiddleNode(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	pa, pb := head, head
 
+	for pb.Next != nil && pb.Next.Next != nil {
+		pa = pa.Next
+		pb = pb.Next.Next
+	}
+
+	return pa
+}
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	var cur = head
+	var prev *ListNode
+
+	for cur != nil {
+		nextNode := cur.Next
+		cur.Next = prev
+
+		prev = cur
+		cur = nextNode
+	}
+
+	return prev
+}
+
+func joinList(ha *ListNode, hb *ListNode) {
+	pa, pb := ha, hb
+
+	for pa != nil && pb != nil {
+		tempA, tempB := pa.Next, pb.Next
+		pa.Next = pb
+		pb.Next = tempA
+
+		pa, pb = tempA, tempB
+	}
+}
+
+func reorderList(head *ListNode) {
+	if head == nil || head.Next == nil {
+		return
+	}
+
+	mid := findMiddleNode(head)
+	l1, l2 := head, mid.Next
+
+	mid.Next = nil
+	l2 = reverseList(l2)
+	joinList(l1, l2)
+	fmt.Println(l1.Val, l2.Val)
 }
 
 // @lc code=end
 
 func main() {
-	stdin := bufio.NewReader(os.Stdin)
-	head := Deserialize[*ListNode](ReadLine(stdin))
+	file, err := os.Open("./testcases.txt")
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	in := bufio.NewReader(file)
+	defer file.Close()
+
+	ReadLine(in)
+	head := Deserialize[*ListNode](ReadLine(in))
 	reorderList(head)
 	ans := head
 
